1e41532d0288b9fd264158ed2a68cc6719fe28f7
thangdangsad <21011631@st.phenikaa-uni.edu.vn>
2023-09-19 06:12:05 +0700

    update
    update

---
+++ search.js
@@ -0,0 +1,17 @@
+ // JavaScript để điều khiển slide show
+ var slideIndex = 0;
+ showSlides();
+
+ function showSlides() {
+     var i;
+     var slides = document.getElementsByClassName("mySlides");
+     for (i = 0; i < slides.length; i++) {
+         slides[i].style.display = "none";
+     }
+     slideIndex++;
+     if (slideIndex > slides.length) {
+         slideIndex = 1;
+     }
+     slides[slideIndex - 1].style.display = "block";
+     setTimeout(showSlides, 2000); // Thay đổi hình ảnh sau 2 giây
+ }
